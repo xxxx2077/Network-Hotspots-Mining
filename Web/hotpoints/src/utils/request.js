494c0073e8b8ba5a8766b4c0c5d04252355e0319
ye-zirong <531794253@qq.com>
2024-06-30 22:28:24 +0800

    前端网络请求改动

--- Web/hotpoints/src/utils/request.js
+++ Web/hotpoints/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // axios实例
 const instance = axios.create({
-    baseURL: '/api',
+    baseURL: 'http://127.0.0.1:8000',
     timeout: 300000
 })
 
--- Web/hotpoints/vue.config.js
+++ Web/hotpoints/vue.config.js
@@ -1,15 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000', //请求后台接口
-        changeOrigin: true, // 允许跨域
-        pathRewrite: {
-          '^/api': '' // 重写请求
-        }
-      }
-    }
-  }
 })
